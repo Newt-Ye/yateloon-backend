dedf3a8f15e1ec3840c9bafe4a183c6bdaa5c2af
Newt-Ye <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>
2025-04-24 17:26:22 +0800

    新增廠別資料

---
+++ src/factories/FactoryEdit.jsx
@@ -0,0 +1,120 @@
+import * as React from "react"
+import {
+  Edit,
+  SimpleForm,
+  TextInput,
+  SaveButton,
+  TextField,
+  /*useTranslate*/
+} from "react-admin"
+import { Box, Typography, Grid, Card, CardContent } from "@mui/material"
+import { validateForm } from "./FactoryCreate"
+
+const FactoryTitle = () => {
+  return <span>{'修改廠別'}</span>;
+};
+
+const FactoryEdit = () => {
+  return (
+    <>
+      <Typography variant="h5" sx={{ mt: 1, color: 'black' }}>
+        廠別資料
+      </Typography>
+      <Edit title={<FactoryTitle/>} redirect={false} mutationMode="optimistic">
+        <SimpleForm
+          // Here for the GQL provider
+          defaultValues={{
+            name: "",
+            code: ""
+          }}
+          validate={validateForm}
+          toolbar={false}
+        >
+          <Box sx={{ display: "flex", justifyContent: "flex-end", width: "100%" }}>
+            <SaveButton />
+          </Box>
+          <Grid container width={{ xs: "100%", xl: 800 }} spacing={2}>
+              <Grid item xs={12}>
+                <TextInput autoFocus source="code" label="廠別代號" isRequired />
+              </Grid>
+              <Grid item xs={12}>
+                <TextInput source="name" label="廠別名稱" isRequired />
+              </Grid>
+          </Grid>
+          <Card sx={{ mt: 4, bgcolor: 'text.disabled', width: '100%' }} >
+            <CardContent>
+              <Grid container spacing={2}>
+                <Grid item xs={6} sm={1}>
+                  <Typography variant="body2" align="left" sx={{ 
+                    color: 'black', 
+                    display: "flex",
+                    justifyContent: "left",
+                    alignItems: "center"}} >
+                    建立者：
+                  </Typography>
+                </Grid>
+                <Grid item xs={6} sm={2}>
+                  <TextField source="creator_name" sx={{ 
+                    color: 'black', 
+                    display: "flex",
+                    justifyContent: "left",
+                    alignItems: "center"}} />
+                </Grid>
+                <Grid item xs={6} sm={1}>
+                  <Typography variant="body2" align="left" sx={{ 
+                    color: 'black', 
+                    display: "flex",
+                    justifyContent: "left",
+                    alignItems: "center"}} >
+                    建立日期：
+                  </Typography>
+                </Grid>
+                <Grid item xs={6} sm={2}>
+                  <TextField source="created_at" sx={{ 
+                    color: 'black', 
+                    display: "flex",
+                    justifyContent: "left",
+                    alignItems: "center"}} />
+                </Grid>
+                <Grid item xs={6} sm={1}>
+                  <Typography variant="body2" align="left" sx={{ 
+                    color: 'black', 
+                    display: "flex",
+                    justifyContent: "left",
+                    alignItems: "center"}} >
+                    修改者：
+                  </Typography>
+                </Grid>
+                <Grid item xs={6} sm={2}>
+                  <TextField source="modifier_name" sx={{ 
+                    color: 'black', 
+                    display: "flex",
+                    justifyContent: "left",
+                    alignItems: "center"}} />
+                </Grid>
+                <Grid item xs={6} sm={1}>
+                  <Typography variant="body2" align="left" sx={{ 
+                    color: 'black', 
+                    display: "flex",
+                    justifyContent: "left",
+                    alignItems: "center"}} >
+                    修改日期：
+                  </Typography>
+                </Grid>
+                <Grid item xs={6} sm={2}>
+                  <TextField source="updated_at" sx={{ 
+                    color: 'black', 
+                    display: "flex",
+                    justifyContent: "left",
+                    alignItems: "center"}} />
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </SimpleForm>
+      </Edit>
+    </>
+  )
+}
+
+export default FactoryEdit
