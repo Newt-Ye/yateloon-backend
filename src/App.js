a048defe5681aabdfee28657bee630a5d65348f5
Newt-Ye <game683001@gmail.com>
2025-05-12 01:03:10 +0800

    調整api呼叫方式

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// import polyglotI18nProvider from 'ra-i18n-polyglot';
-import { 
+import { useEffect, useState } from "react";
+import {
   Admin,
   Resource,
   localStorageStore,
   StoreContextProvider,
   defaultTheme,
 } from "react-admin";
-import customDataProvider from "./customDataProvider";
+import customDataProviderFactory from "./customDataProvider"; // 改成 factory function
 import authProvider from './authProvider';
 import inventoryItemCategories from './inventoryItemCategories';
 import inventoryItems from './inventoryItems';
@@ -21,195 +21,56 @@ import currencies from './currencies';
 import currencyExchangeRates from './currencyExchangeRates';
 import paymentTerms from './paymentTerms';
 import i18nProvider from './i18n';
-import { Layout, Login } from "./layout"
-// import en from './i18n/en';
-// import zhTW from './i18n/zh-TW';
-// import zhCN from './i18n/zh-CN';
-import { useState, useEffect } from "react";
+import { Layout, Login } from "./layout";
 
-// const i18nProvider = polyglotI18nProvider(locale => 
-//   locale === 'zh-tw' ? zhTW : locale === 'zh-cn' ? zhCN : en,
-//   'zh-tw', // Default locale
-//   [
-//       { locale: 'en', name: 'English' },
-//       { locale: 'zh-tw', name: '繁體中文' },
-//       { locale: 'zh-cn', name: '簡體中文' }
-//   ],
-// );
+const store = localStorageStore(undefined, "ECommerce");
 
-const store = localStorageStore(undefined, "ECommerce")
-
-const App = () => {
+const AppWrapper = () => {
+  const [dataProvider, setDataProvider] = useState(null);
   const [accesses, setAccesses] = useState(null);
 
   useEffect(() => {
-    authProvider.getPermissions().then(setAccesses);
+    Promise.all([
+      fetch("/config.json").then(res => res.json()),
+      authProvider.getPermissions()
+    ]).then(([config, access]) => {
+      const apiUrl = config.REACT_APP_API_URL;
+      const provider = customDataProviderFactory(apiUrl);
+      setDataProvider(provider);
+      setAccesses(access);
+    });
   }, []);
 
-  if (!accesses) return null
+  if (!dataProvider || !accesses) return null;
 
   return (
-    <Admin
-      title=""
-      store={store}
-      dataProvider={customDataProvider}
-      authProvider={authProvider}
-      loginPage={Login}
-      i18nProvider={i18nProvider}
-      layout={Layout}
-      defaultTheme={defaultTheme}
-      dashboard={() => <div></div>}
-    >
-      {/* {(accesses === 'superuser' || accesses?.["inventory-item-categories"]?.view) && (
-        <Resource
-          name="inventory-item-categories"
-          list={
-            accesses === 'superuser' || accesses?.["inventory-item-categories"]?.view
-              ? inventoryItemCategories.list
-              : null
-          }
-          create={
-            accesses === 'superuser' || accesses?.["inventory-item-categories"]?.create
-              ? inventoryItemCategories.create
-              : null
-          }
-          edit={
-            accesses === 'superuser' || accesses?.["inventory-item-categories"]?.edit
-              ? inventoryItemCategories.edit
-              : null
-          }
-          show={
-            accesses === 'superuser' || accesses?.["inventory-item-categories"]?.view
-              ? inventoryItemCategories.show
-              : null
-          }
-        />
-      )}
-      {(accesses === 'superuser' || accesses?.["inventory-items"]?.view) && (
-        <Resource
-          name="inventory-items"
-          list={
-            accesses === 'superuser' || accesses?.["inventory-items"]?.view
-              ? inventoryItems.list
-              : null
-          }
-          create={
-            accesses === 'superuser' || accesses?.["inventory-items"]?.create
-              ? inventoryItems.create
-              : null
-          }
-          edit={
-            accesses === 'superuser' || accesses?.["inventory-items"]?.edit
-              ? inventoryItems.edit
-              : null
-          }
-        />
-      )}
-      {(accesses === 'superuser' || accesses?.["companies"]?.view) && (
-        <Resource
-          name="companies"
-          list={
-            accesses === 'superuser' || accesses?.["companies"]?.view
-              ? companies.list
-              : null
-          }
-          create={
-            accesses === 'superuser' || accesses?.["companies"]?.create
-              ? companies.create
-              : null
-          }
-          edit={
-            accesses === 'superuser' || accesses?.["companies"]?.edit
-              ? companies.edit
-              : null
-          }
-          show={
-            accesses === 'superuser' || accesses?.["companies"]?.view
-              ? companies.show
-              : null
-          }
-        />
-      )}
-      {(accesses === 'superuser' || accesses?.["users"]?.view) && (
-        <Resource
-          name="users"
-          list={
-            accesses === 'superuser' || accesses?.["users"]?.view
-              ? users.list
-              : null
-          }
-          create={
-            accesses === 'superuser' || accesses?.["users"]?.create
-              ? users.create
-              : null
-          }
-          edit={
-            accesses === 'superuser' || accesses?.["users"]?.edit
-              ? users.edit
-              : null
-          }
-        />
-      )}
-      {(accesses === 'superuser' || accesses?.["departments"]?.view) && (
-        <Resource
-          name="departments"
-          list={
-            accesses === 'superuser' || accesses?.["departments"]?.view
-              ? departments.list
-              : null
-          }
-          create={
-            accesses === 'superuser' || accesses?.["departments"]?.create
-              ? departments.create
-              : null
-          }
-          edit={
-            accesses === 'superuser' || accesses?.["departments"]?.edit
-              ? departments.edit
-              : null
-          }
-          show={
-            accesses === 'superuser' || accesses?.["departments"]?.view
-              ? departments.show
-              : null
-          }
-        />
-      )}
-      {(accesses === 'superuser' || accesses?.["permissions"]?.view) && (
-        <Resource
-          name="permissions"
-          list={
-            accesses === 'superuser' || accesses?.["permissions"]?.view
-              ? permissions.list
-              : null
-          }
-          edit={
-            accesses === 'superuser' || accesses?.["permissions"]?.edit
-              ? permissions.edit
-              : null
-          }
-        />
-      )} */}
-      <Resource name="inventory-item-categories" {...inventoryItemCategories} />
-      <Resource name="inventory-items" {...inventoryItems} />
-      <Resource name="companies" {...companies} />
-      <Resource name="users" {...users} />
-      <Resource name="departments" {...departments} />
-      <Resource name="permissions" {...permissions} />
-      <Resource name="settings" {...settings} />
-      <Resource name="factories" {...factories} />
-      <Resource name="warehouses" {...warehouses} />
-      <Resource name="currencies" {...currencies} />
-      <Resource name="currency-exchange-rates" {...currencyExchangeRates} />
-      <Resource name="payment-terms" {...paymentTerms} />
-    </Admin>
-  )
-}
-
-const AppWrapper = () => (
-  <StoreContextProvider value={store}>
-    <App />
-  </StoreContextProvider>
-)
+    <StoreContextProvider value={store}>
+      <Admin
+        title=""
+        store={store}
+        dataProvider={dataProvider}
+        authProvider={authProvider}
+        loginPage={Login}
+        i18nProvider={i18nProvider}
+        layout={Layout}
+        defaultTheme={defaultTheme}
+        dashboard={() => <div></div>}
+      >
+        <Resource name="inventory-item-categories" {...inventoryItemCategories} />
+        <Resource name="inventory-items" {...inventoryItems} />
+        <Resource name="companies" {...companies} />
+        <Resource name="users" {...users} />
+        <Resource name="departments" {...departments} />
+        <Resource name="permissions" {...permissions} />
+        <Resource name="settings" {...settings} />
+        <Resource name="factories" {...factories} />
+        <Resource name="warehouses" {...warehouses} />
+        <Resource name="currencies" {...currencies} />
+        <Resource name="currency-exchange-rates" {...currencyExchangeRates} />
+        <Resource name="payment-terms" {...paymentTerms} />
+      </Admin>
+    </StoreContextProvider>
+  );
+};
 
 export { AppWrapper as App };
